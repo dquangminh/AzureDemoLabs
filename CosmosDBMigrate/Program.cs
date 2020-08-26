41a83d66e41b08980dc3bf65e8f562ab7a64f704
dquangminh <dquangminh1310@gmail.com>
2020-08-26 16:44:26 +0700

    5th commit

---
+++ CosmosDBMigrate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CosmosDBMigrate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
