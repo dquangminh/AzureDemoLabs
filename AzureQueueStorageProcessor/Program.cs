823a228cea13b9e4d0772bbd22d241504513c37e
dquangminh <dquangminh1310@gmail.com>
2020-09-01 14:16:46 +0700

    14th commit

---
+++ AzureQueueStorageProcessor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Azure;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System.Threading.Tasks;
+
+namespace AzureQueueStorageProcessor
+{
+    class Program
+    {
+        private const string storageConnectionString = "";
+        private const string queueName = "myqueues";
+        static async Task Main(string[] args)
+        {
+            QueueClient client = new QueueClient(storageConnectionString, queueName);        
+            await client.CreateAsync();
+
+            await Console.Out.WriteLineAsync($"Account Uri:\t{client.Uri}");
+            int batchSize = 10;
+            TimeSpan visibilityTimeout = TimeSpan.FromSeconds(2.5d);
+
+            Response<QueueMessage[]> messages = await client.ReceiveMessagesAsync(batchSize, visibilityTimeout);
+            foreach(QueueMessage message in messages?.Value)
+            {
+                Console.WriteLine($"[{message.MessageId}]\t{message.MessageText}");
+            }
+        }
+    }
+}
---
+++ EventGridPublisher/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Azure.EventGrid;
+using Microsoft.Azure.EventGrid.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EventGridPublisher
+{
+    class Program
+    {
+        private const string topicEndpoint = "";
+        private const string topicKey = "";
+        static async Task Main(string[] args)
+        {
+            TopicCredentials credentials = new TopicCredentials(topicKey);
+            EventGridClient client = new EventGridClient(credentials);
+
+            List<EventGridEvent> events = new List<EventGridEvent>();
+
+            var firstPerson = new 
+            {
+                FullName = "Alba Sutton",
+                Address = "4567 Pine Avenue, Edison, WA 97202"
+            };
+
+            EventGridEvent firstEvent = new EventGridEvent
+            {
+                Id = Guid.NewGuid().ToString(),
+                EventType = "Employees.Registration.New",
+                EventTime = DateTime.Now,
+                Subject = $"New Employee : {firstPerson.FullName}",
+                Data = firstPerson.ToString(),
+                DataVersion = "1.0.0"
+            };
+
+            events.Add(firstEvent);
+
+            string topicHostName = new Uri(topicEndpoint).Host;
+            await client.PublishEventsAsync(topicHostName, events);
+
+            await Console.Out.WriteLineAsync("Events published");
+        }
+    }
+}
